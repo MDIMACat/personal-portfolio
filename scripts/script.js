678b2ca4913ec49c5ba29860f1311dbc8645a538
MDIMACat <romanmdima@gmail.com>
2024-06-28 22:14:12 +0200

    added the hamburger nav

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('nav')[0];
+const navLinks = navbarLinks.getElementsByTagName('a');
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+});
+
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', () => {
+        navbarLinks.classList.remove('active');
+    });
+}
