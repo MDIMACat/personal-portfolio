2646f6cf5f4e42eb708152daa8f5804192f4e66f
MDIMACat <romanmdima@gmail.com>
2024-08-07 19:52:29 +0200

    Second attempt

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,73 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('nav')[0];
-const navLinks = navbarLinks.getElementsByTagName('a');
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("nav")[0];
+const navLinks = navbarLinks.getElementsByTagName("a");
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
 });
 
-for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener('click', () => {
-        navbarLinks.classList.remove('active');
-    });
-    
+function getUserLocation() {
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 10000,
+  };
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      successCallback,
+      errorCallback,
+      options
+    );
+  } else {
+    console.error("Geolocation  is not supported by   your browser.");
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const weatherApiUrl = "https://api.open-meteo.com/v1/forecast?latitude=-28.783&longitude=32.0377&current=apparent_temperature&timezone=Africa%2FCairo";
+const successCallback = (position) => {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
 
-    fetch(weatherApiUrl)
-    .then(response => response.json())
-    .then(data => {
-        const temp = data.current.apparent_temperature;
-        const tempElement = document.getElementById('temp');
-        if (tempElement) {
-            tempElement.innerHTML = temp + "°C";
-        }
+  const weatherApiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=apparent_temperature&timezone=Africa%2FCairo`;
+
+  fetch(weatherApiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const temp = data.current.apparent_temperature;
+      const tempElement = document.getElementById("temp");
+      if (tempElement) {
+        tempElement.innerHTML = temp + "°C";
+      }
     })
-    .catch(error => {
-        console.log(error);
+    .catch((error) => {
+      const tempElement = document.getElementById("temp");
+      tempElement.innerHTML = "No data available";
+      console.error("Error fetching weather data:", error);
     });
+};
 
-    
-});
+const errorCallback = (error) => {
+  console.log(error);
+};
 
-document.addEventListener('DOMContentLoaded', function() {
-    const newsUrl = "https://newsdata.io/api/1/latest?language=en&apikey=pub_50223ff1354b8802cf901dfce78fd4cc91feb"
+getUserLocation();
 
-    fetch(newsUrl)
-    .then(response => response.json())
-    .then(data => {
+const newsUrl =
+  "https://newsdata.io/api/1/latest?language=en&apikey=pub_50223ff1354b8802cf901dfce78fd4cc91feb";
 
-        for (let i = 0; i < data.results.length; i++) {
-            const title = data.results[i].title;
-            const link = data.results[i].link;
-            const newsLink = document.querySelector('.headline-content a');
-    
-            if (newsLink) {
-                newsLink.href = link;
-                newsLink.innerHTML = data.results[i].title;
-            }
-        }
-    })  
-});
+fetch(newsUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.results.length; i++) {
+      const title = data.results[i].title;
+      const link = data.results[i].link;
+      const newsLink = document.querySelector(".headline-content a");
+
+      if (newsLink) {
+        newsLink.href = link;
+        newsLink.innerHTML = data.results[i].title;
+      }
+    }
+  })
+  .catch((error) => {
+    console.error("Error fetching news data:", error);
+   
+  });
